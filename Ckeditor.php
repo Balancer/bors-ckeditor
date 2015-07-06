fe6617f9d0e5561b75f2500586dab4ae7162f1ad
Balancer <balancer@balancer.ru>
2015-07-06 07:27:53 +0300

    correct path

--- Ckeditor.php
+++ Ckeditor.php
@@ -4,6 +4,8 @@ class Ckeditor
 {
 	static function appear($element, $params = array())
 	{
+		bors_use('/_bors-assets/bower/ckeditor/ckeditor.js');
+
 		template_js('
 $().ready(function() {
 	$("'.$element.'").ckeditor({
